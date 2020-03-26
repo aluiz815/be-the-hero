ce2a124b3810eb491e4789a71ad481ecb2a67d8e
Andre Luiz <aluiz815@gmail.com>
2020-03-26 00:01:53 -0300

    Deploy

--- backend/src/database/connection.js
+++
@@ -1,6 +0,0 @@
-const knex = require('knex')
-require('dotenv/config')
-const env = process.env.NODE_ENV || 'development'
-const configuration = require('../../knexfile')[env]
-const connection = knex(configuration)
-module.exports = connection
